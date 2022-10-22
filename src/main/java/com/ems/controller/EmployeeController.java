c57775089a3cbdd0804af448dbfdc595830f8fc7
Bhavya <bhavyamathur601@gmail.com>
2022-10-22 16:36:05 +0530

    init commit

---
+++ src/main/java/com/ems/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.ems.controller;
+
+import com.ems.model.Employee;
+import com.ems.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+
+}
---
+++ src/main/java/com/ems/model/Employee.java
@@ -0,0 +1,61 @@
+package com.ems.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    @Column(name = "EmployeeAge")
+    private int age;
+
+
+    public Employee() {
+    }
+
+    public Employee(long id, String firstName, String lastName, String emailId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
